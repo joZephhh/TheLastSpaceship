46938a35c06e168ca1ea13589cf590af59ad83be
joZephhh <joseph.q@me.com>
2016-12-12 23:47:20 +0100

    comments and title score

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,6 +5,7 @@ var tls = {}
 
 tls.el = {} // DOM
 tls.el.container = document.querySelector(".container-game");
+
 //views
 tls.el.launch_page = tls.el.container.querySelector(".landing-page");
 tls.el.game = tls.el.container.querySelector(".game");
@@ -16,9 +17,12 @@ tls.el.svgPersoParts = tls.el.spaceship.querySelectorAll(".spaceship-personalisa
 tls.el.dataTotal= tls.el.game.querySelector(".game-total-data span");
 tls.el.dataSpeed = tls.el.game.querySelector(".game-data-speed .game-data-info span");
 tls.el.dataMoney = tls.el.game.querySelector(".game-data-money .game-data-info span");
-//data
+
+
+//DATA
 tls.data = {}
 
+// if player has already play
 if (localStorage.getItem("tls_distance")) {
 	tls.data.distance = parseFloat(localStorage.getItem("tls_distance"));
 }
@@ -32,8 +36,12 @@ if (localStorage.getItem("tls_distance_total")) {
 else {
 tls.data.distanceTotal = 0;
 }
+
+// score multiplicator
 tls.data.multi_distance=1;
+//speed of the starts
 tls.data.speed = 1;
+// user money
 tls.data.money = 0;
 // buttons
 tls.el.start_btn = tls.el.launch_page.querySelector(".ld-start");
@@ -139,19 +147,6 @@ function loop()
 
 
 
-function restore() {
-	var actualDate = new Date();
-	var pastDate = parseFloat(localStorage.getItem("tls_date"))
-	var differenceDate = Math.round(((actualDate.getTime() - pastDate) / 1000));
-	if(tls.data.distance-differenceDate > 0) {
-			tls.data.distance -= differenceDate;
-	}
-	else {
-		tls.data.distance=0;
-	}
-
-	console.log(differenceDate, tls.data.distance)
-}
 //events
 tls.el.start_btn.addEventListener("click", function(e) {
 	restore();
@@ -169,6 +164,8 @@ tls.el.start_btn.addEventListener("click", function(e) {
 
 	},5000)
 })
+
+// test svg change color
 tls.el.colorInput.addEventListener("blur",function(){
 	for (var i = 0; i < tls.el.svgPersoParts.length; i++) {
 		tls.el.svgPersoParts[i].style="fill:"+tls.el.colorInput.value;
@@ -180,27 +177,43 @@ add_distance();
 console.log("spaceship clicked")
 })
 
+// get points where we was away
+function restore() {
+	var actualDate = new Date();
+	var pastDate = parseFloat(localStorage.getItem("tls_date"))
+	var differenceDate = Math.round(((actualDate.getTime() - pastDate) / 1000));
+	if(tls.data.distance-differenceDate > 0) {
+			tls.data.distance -= differenceDate;
+	}
+	else {
+		tls.data.distance=0;
+	}
+}
 
+// add speed
 function add_distance() {
 	tls.data.distance += (1*tls.data.multi_distance);
 	tls.el.dataSpeed.innerText=tls.data.distance;
 	tls.el.dataTotal.innerText=tls.data.distanceTotal;
 }
+
+// calc the total distance
 setInterval(function() {
 	tls.data.distanceTotal+= (1*tls.data.distance);
 	tls.el.dataTotal.innerText=tls.data.distanceTotal;
 },1000)
+
+// discrease speed
 function remove_distance() {
 if(tls.data.distance> 0) {
 	tls.data.distance -= 1;
 tls.el.dataSpeed.innerText=tls.data.distance;
 }
-
 }
 
 
 
-
+// when nothing move
 var stationary;
 var timeoutStandby;
 
@@ -214,10 +227,12 @@ timeoutStandby = setTimeout(function() {
 },3000)
 }
 
+// show the money
 function money() {
 tls.el.dataMoney.innerText = tls.data.money;
 }
 
+// update all the data stuff
 function checkData() {
 	tls.data.multi_distance = 1+ Math.floor((tls.data.distance/75))
 	if(tls.data.speed <=40) {
@@ -227,25 +242,16 @@ function checkData() {
 		tls.data.speed = 40;
 	}
 	tls.data.money = Math.floor(tls.data.distanceTotal/1000)
-	money()
+	money();
+	document.title = "TLS : "+ tls.data.distance +" an. lu. /sec.";
 	localStorage.setItem("tls_distance_total", tls.data.distanceTotal);
 	localStorage.setItem("tls_distance", tls.data.distance);
 	var actualDate = new Date();
 	localStorage.setItem("tls_date", actualDate.getTime());
 }
+
+// loop to launch update
 function loop_data() {
 	window.requestAnimationFrame(loop_data);
 	checkData();
 }
-
-// function saveChanges () {
-// 	var actualDate = new Date();
-//     localStorage.setItem("tls_date", actualDate.getTime());
-//     window.onbeforeunload = null;
-// }
-//
-// function exitConfirmation () {
-//     setTimeout( saveChanges, 0 );
-// }
-//
-// window.onbeforeunload = exitConfirmation();
