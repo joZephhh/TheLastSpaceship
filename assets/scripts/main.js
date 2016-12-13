3087611fb9e605fbda7ab5d25bb9c470ce276222
joZephhh <joseph.q@me.com>
2016-12-13 13:43:12 +0100

    max value

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,34 @@
 console.log("script linked");
+var tls_dtb;
+if(localStorage.getItem('tls_database')) {
+	 tls_dtb = JSON.parse(localStorage.getItem('tls_database'));
+}
+else {
+tls_dtb ={
+		"objects" : {
+			"tank" : {
+				"lvl" : 1,
+				"dimSpeed" : 2,
+				"ratio" : 1.2
+			},
+			"reactors": {
+				"lvl" : 1,
+				"maxSpeed" :1000,
+				"ratio" : 1.3
+			},
+			"starsContainer": {
+				"lvl" : 1,
+				"canContain" :75,
+				"ratio" : 2
+			}
+		}
+	}
+}
+
+
+
+
+
 // DOM Manipulation
 
 var tls = {}
@@ -13,6 +43,9 @@ tls.el.game = tls.el.container.querySelector(".game");
 //elements
 tls.el.colorInput = tls.el.container.querySelector(".colorSpaceship");
 tls.el.spaceship = tls.el.game.querySelector(".game-spaceship svg");
+tls.el.unitTotal = tls.el.game.querySelector(".units-total");
+tls.el.unitSecNb = tls.el.game.querySelector(".unit-sec-nb");
+tls.el.unitSec = tls.el.game.querySelector(".units-sec");
 tls.el.svgPersoParts = tls.el.spaceship.querySelectorAll(".spaceship-personalisation");
 tls.el.dataTotal= tls.el.game.querySelector(".game-total-data span");
 tls.el.dataSpeed = tls.el.game.querySelector(".game-data-speed .game-data-info span");
@@ -36,11 +69,17 @@ if (localStorage.getItem("tls_distance_total")) {
 else {
 tls.data.distanceTotal = 0;
 }
+if (localStorage.getItem("tls_distance_stars")) {
+	tls.data.distanceStars = parseFloat(localStorage.getItem("tls_distance_stars"));
+}
+else {
+tls.data.distanceStars = 0;
+}
 
 // score multiplicator
 tls.data.multi_distance=1;
 //speed of the starts
-tls.data.speed = 1;
+tls.data.speed = 0.5;
 // user money
 tls.data.money = 0;
 // buttons
@@ -165,12 +204,7 @@ tls.el.start_btn.addEventListener("click", function(e) {
 	},5000)
 })
 
-// test svg change color
-tls.el.colorInput.addEventListener("blur",function(){
-	for (var i = 0; i < tls.el.svgPersoParts.length; i++) {
-		tls.el.svgPersoParts[i].style="fill:"+tls.el.colorInput.value;
-	}
-})
+
 tls.el.spaceship.addEventListener("click", function() {
 rebootStationary();
 add_distance();
@@ -182,8 +216,8 @@ function restore() {
 	var actualDate = new Date();
 	var pastDate = parseFloat(localStorage.getItem("tls_date"))
 	var differenceDate = Math.round(((actualDate.getTime() - pastDate) / 1000));
-	if(tls.data.distance-differenceDate > 0) {
-			tls.data.distance -= differenceDate;
+	if(tls.data.distance-differenceDate*100 > 0) {
+			tls.data.distance -= differenceDate*100;
 	}
 	else {
 		tls.data.distance=0;
@@ -192,15 +226,25 @@ function restore() {
 
 // add speed
 function add_distance() {
+	var maxSpeed = tls_dtb.objects.reactors.maxSpeed;
+	if(tls.data.distance +  (1*tls.data.multi_distance) < maxSpeed) {
 	tls.data.distance += (1*tls.data.multi_distance);
-	tls.el.dataSpeed.innerText=tls.data.distance;
-	tls.el.dataTotal.innerText=tls.data.distanceTotal;
+		tls.el.dataSpeed.style="color:#2ecc71";
+		tls.el.unitSecNb.classList.remove("unit-max");
+}
+else {
+	tls.data.distance = maxSpeed
+		tls.el.dataSpeed.style="color:orange";
+		tls.el.unitSecNb.classList.add("unit-max");
+}
+
 }
 
 // calc the total distance
 setInterval(function() {
 	tls.data.distanceTotal+= (1*tls.data.distance);
-	tls.el.dataTotal.innerText=tls.data.distanceTotal;
+	tls.data.distanceStars+= (1*tls.data.distance);
+	money();
 },1000)
 
 // discrease speed
@@ -208,6 +252,8 @@ function remove_distance() {
 if(tls.data.distance> 0) {
 	tls.data.distance -= 1;
 tls.el.dataSpeed.innerText=tls.data.distance;
+	tls.el.dataSpeed.style="color:#e74c3c";
+	tls.el.unitSecNb.classList.remove("unit-max");
 }
 }
 
@@ -223,17 +269,56 @@ clearTimeout(timeoutStandby);
 timeoutStandby = setTimeout(function() {
 	 stationary = setInterval(function () {
 		remove_distance();
-	}, 1000);
+	}, 10);
 },3000)
 }
 
 // show the money
 function money() {
+	if(tls.data.money < tls_dtb.objects.starsContainer.canContain ) {
+	tls.data.money = Math.floor(tls.data.distanceStars/1000)
+}
+else {
+	tls.data.money = tls_dtb.objects.starsContainer.canContain;
+}
 tls.el.dataMoney.innerText = tls.data.money;
 }
+function buy(price) {
+	if (tls.data.distanceStars-price >= 0) {
+		tls.data.distanceStars-=price;
+	}
+
+}
+function unitsTotal() {
+	if (tls.data.distanceTotal > 0 && tls.data.distanceTotal < 1000) {
+		tls.el.dataTotal.innerText=tls.data.distanceTotal;
+		tls.el.unitTotal.innerText="mètres parcourus"
+	}
+	else if (tls.data.distanceTotal > 1000) {
+		tls.el.dataTotal.innerText=Math.round(tls.data.distanceTotal/1000);
+		tls.el.unitTotal.innerText="kilomètres parcourus";
+
+	}
+
+}
+
+function unitsSec() {
+	if (tls.data.distance >= 0 && tls.data.distance < 1000) {
+		tls.el.dataSpeed.innerText=tls.data.distance;
+		tls.el.unitSec.innerText="mètres /s"
+	}
+	else if (tls.data.distanceTotal > 1000) {
+		tls.el.dataSpeed.innerText=Math.round((tls.data.distance/1000)*10)/10;
+		tls.el.unitSec.innerText="kilomètres /s"
+	}
+}
+
+
+
 
 // update all the data stuff
 function checkData() {
+	console.log(tls.data.distanceTotal);
 	tls.data.multi_distance = 1+ Math.floor((tls.data.distance/75))
 	if(tls.data.speed <=40) {
 		tls.data.speed =  1+ Math.floor((tls.data.distance/5)/20);
@@ -241,17 +326,46 @@ function checkData() {
 	else {
 		tls.data.speed = 40;
 	}
-	tls.data.money = Math.floor(tls.data.distanceTotal/1000)
-	money();
+
+
+	unitsSec();
+	unitsTotal();
 	document.title = "TLS : "+ tls.data.distance +" an. lu. /sec.";
-	localStorage.setItem("tls_distance_total", tls.data.distanceTotal);
-	localStorage.setItem("tls_distance", tls.data.distance);
-	var actualDate = new Date();
-	localStorage.setItem("tls_date", actualDate.getTime());
+	// localStorage.setItem("tls_distance_total", tls.data.distanceTotal);
+	// localStorage.setItem("tls_distance_stars", tls.data.distanceStars);
+	// localStorage.setItem("tls_distance", tls.data.distance);
+	// var actualDate = new Date();
+	// localStorage.setItem("tls_date", actualDate.getTime());
 }
 
 // loop to launch update
 function loop_data() {
 	window.requestAnimationFrame(loop_data);
 	checkData();
+}
+
+// BETA
+function tls_reset() {
+	localStorage.setItem("tls_distance_total", 0);
+	localStorage.setItem("tls_distance_stars", 0);
+	localStorage.setItem("tls_distance", 0);
+	console.log("done")
+
+}
+
+// test svg change color
+tls.el.colorInput.addEventListener("blur",function(){
+	for (var i = 0; i < tls.el.svgPersoParts.length; i++) {
+		tls.el.svgPersoParts[i].style="fill:"+tls.el.colorInput.value;
+	}
+})
+
+document.querySelector(".testShop").addEventListener("click", function() {
+upgradeSpeed();
+})
+
+function upgradeSpeed() {
+	tls_dtb.objects.reactors.maxSpeed =  tls_dtb.objects.reactors.maxSpeed*tls_dtb.objects.reactors.ratio
+	tls_dtb.objects.reactors.level +=1
+	localStorage.setItem('tls_database', JSON.stringify(tls_dtb));
 }
